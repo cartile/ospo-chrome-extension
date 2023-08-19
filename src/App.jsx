3b019cd3253ff65d1f64d76610912a0bd4106bb7
Jeffrey Wang <wangjeffrey30@gmail.com>
2023-08-18 22:17:16 -0400

    gmailParsingWorks

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import './reset.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const func = () => {
+    console.log("hello")
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button onClick={func}>Observe Site</button>
+      <h1 id="extensionTitle">ospo</h1>
+      <span>
+        <h3>Company:</h3> 
+        <p>Amazon</p>
+      </span>
+      <span>
+        <h3>Role:</h3> 
+        <p>Software Engineering Intern</p>
+      </span>
+      
+      <button>Log</button>
+      <div id="somethingWrongContainer">
+        <p>Something wrong?</p>
+        <p><strong>Edit Details</strong></p>
+        <p>or</p>
+        <p><strong>Delete Record</strong></p>
+      </div>
     </div>
   )
 }
---
+++ src/logic/gmailParse.js
@@ -0,0 +1,73 @@
+let emailLogged = false;
+let checkInterval = null;
+let currentObserver = null;
+
+function checkForEmailContent() {
+    if (emailLogged) return;
+
+    const paragraphs = document.querySelectorAll('.nH.a98.iY');
+    if (paragraphs.length > 0) {
+        console.log('Found email content.');
+        paragraphs.forEach(p => {
+            console.log(p.innerText);
+        });
+        emailLogged = true;
+        if (checkInterval) {
+            clearInterval(checkInterval);
+            checkInterval = null;
+        }
+        if (currentObserver) {
+            currentObserver.disconnect();
+            currentObserver = null;
+        }
+    }
+}
+
+function observeDOMChanges() {
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    if (currentObserver) {
+        currentObserver.disconnect();
+    }
+
+    currentObserver = new MutationObserver(checkForEmailContent);
+    currentObserver.observe(document, { childList: true, subtree: true });
+}
+
+window.onhashchange = function() {
+    console.log('URL hash changed to:', window.location.hash);
+    if (window.location.hash.includes('#inbox/')) {
+        console.log('Starting to check for email content...');
+        emailLogged = false;
+        observeDOMChanges();
+        if (checkInterval) {
+            clearInterval(checkInterval);
+        }
+        checkInterval = setInterval(checkForEmailContent, 1000);
+    } else {
+        if (checkInterval) {
+            clearInterval(checkInterval);
+            checkInterval = null;
+        }
+        if (currentObserver) {
+            currentObserver.disconnect();
+            currentObserver = null;
+        }
+    }
+}
+
+// Check once the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.hash.includes('#inbox/')) {
+        observeDOMChanges();
+        checkForEmailContent();
+    }
+});
+
+// Check on window load for extra assurance
+window.onload = function() {
+    if (window.location.hash.includes('#inbox/')) {
+        observeDOMChanges();
+        checkForEmailContent();
+    }
+};
